c3a1e050c5ae6544e5b369555a47e4e3b0a3fab9
Jonathan Calles <jcallesmilla@gmail.com>
2025-10-12 16:44:59 -0500

    Eliminando archivo 2

--- archivo2.java
+++
@@ -1,7 +0,0 @@
-public class archivo2 {
-    public static void main(String[] args) {
-		System.out.println("Hello World 2");
-        System.out.println("Prueba de texto");
-        System.out.println("Otro cambio");
-	}
-}
