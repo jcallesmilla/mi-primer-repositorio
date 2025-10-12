c1a07189bf14d71115b7c9950e70de8c74237de4
Jonathan Calles <jcallesmilla@gmail.com>
2025-10-12 15:20:51 -0500

    Agregando cambios en archivo2.java

--- archivo2.java
+++ archivo2.java
@@ -2,5 +2,6 @@ public class archivo2 {
     public static void main(String[] args) {
 		System.out.println("Hello World 2");
         System.out.println("Prueba de texto");
+        System.out.println("Otro cambio");
 	}
 }
